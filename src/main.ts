73343198a5dd628ad1eebb40c615cabfaab0b99f
Sergio <Sergio.Alvarez@ibm.com>
2021-12-18 19:38:59 -0600

    preparing app for heroku

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-let initialPort = 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  let shouldTryAgain = true;
-
-  while (shouldTryAgain) {
-    try {
-      await app.listen(initialPort);
-      shouldTryAgain = false;
-      console.log('listening on ' + initialPort);
-    } catch (e) {
-      shouldTryAgain = true;
-      initialPort++;
-    }
-  }
+  app.enableCors()
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

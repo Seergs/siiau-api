a8e91c0683e2c63b349a1ecf0ba329c92c07a098
Sergio <Sergio.Alvarez@ibm.com>
2021-12-27 11:05:47 -0600

    Adding security API key to analytics endpoints

--- src/database/database.controller.ts
+++ src/database/database.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Headers, ForbiddenException } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
 @Controller('database')
 export class DatabaseController {
   constructor(private readonly databaseService: DatabaseService) {}
   @Get('/analytics')
-  getAll() {
+  getAll(@Headers("x-api-key") apiKey: string) {
+    if (!apiKey) throw new ForbiddenException("No API key provided");
+    if (apiKey !== process.env.API_KEY) throw new ForbiddenException("Invalid API key");
     return this.databaseService.getAll();
   }
   @Get('/analytics/summary')
-  getSummary() {
+  getSummary(@Headers("x-api-key") apiKey: string) {
+    if (!apiKey) throw new ForbiddenException("No API key provided");
+    if (apiKey !== process.env.API_KEY) throw new ForbiddenException("Invalid API key");
     return this.databaseService.getSummary();
   }
 }

d7076d419a0001d21746c763ddcfc0fe8ee84332
Sergio <Sergio.Alvarez@ibm.com>
2022-01-20 20:46:40 -0600

    add persistance for admission

--- src/admission/admission.controller.ts
+++ src/admission/admission.controller.ts
@@ -1,20 +1,28 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { ApiHeaders, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 import { Page } from 'puppeteer';
+import { DatabaseService } from 'src/database/database.service';
 import { AdmissionService } from './admission.service';
 import { RootResponse, RootHeaders } from './swagger';
 
 @ApiTags('admission')
 @Controller('admission')
 export class AdmissionController {
-  constructor(private readonly admissionService: AdmissionService) {}
+  constructor(
+    private readonly admissionService: AdmissionService,
+    private readonly databaseService: DatabaseService,
+  ) {}
 
   @ApiResponse(RootResponse)
   @ApiHeaders(RootHeaders)
   @Get()
-  getAdmissionInfo(@Res({ passthrough: true }) res: Response) {
+  async getAdmissionInfo(
+    @Res({ passthrough: true }) res: Response,
+    @Req() request: Request,
+  ) {
     const page = res.locals.page as Page;
+    this.databaseService.save('admission', request.url);
     return this.admissionService.getAdmissionInformation(page);
   }
 }
